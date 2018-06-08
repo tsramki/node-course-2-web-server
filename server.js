2ad68fb389c92b09ecf73a43100b99c1f76aa022
Ramakrishna Seshadri <someone@somewhere.com>
2018-06-07 17:19:31 -0700

    'Add project page'

--- server.js
+++ server.js
@@ -53,6 +53,14 @@ app.get('/about',(req,res) => {
   });
 });
 
+app.get('/projects',(req,res) => {
+  //  res.send('About Express');
+  res.render('projects.hbs',{
+    pageTitle: 'Projects Page'
+  });
+});
+
+
 app.get('/bad',(req,res) => {
   res.send({
     errorMessage: 'Unable to fulfil your request'
